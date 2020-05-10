4993b6b8a7adfacf9d000fc116b027e2f2169237
Albert Garcia <albertgarcia@MBP-de-Albert.home>
2020-05-10 19:48:03 +0200

    funciones 1er nivel

---
+++ funciones1nivel.py
@@ -0,0 +1,21 @@
+def normal(i):
+    return i
+
+def cuadrado(x):
+    return x * x
+
+def sumaTodos(limitTo, f):
+    resultado = 0
+    for i in range(0, limitTo):
+        resultado += f(i)
+        
+    return resultado
+        
+print(sumaTodos(100, normal))
+print(sumaTodos(3, cuadrado))
+
+
+  
+        
+        
+        
