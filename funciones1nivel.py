7c8754375909b7c4507d1010eeaee2eb8a0e36c8
Albert Garcia <albertgarcia@MBP-de-Albert.home>
2020-05-10 20:40:42 +0200

    lambda

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,15 +4,20 @@ def normal(i):
 def cuadrado(x):
     return x * x
 
+def cubo(x):
+    return x**3
+
+
 def sumaTodos(limitTo, f):
     resultado = 0
-    for i in range(0, limitTo):
+    for i in range(0, limitTo+1):
         resultado += f(i)
         
     return resultado
-        
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
 
 
   
